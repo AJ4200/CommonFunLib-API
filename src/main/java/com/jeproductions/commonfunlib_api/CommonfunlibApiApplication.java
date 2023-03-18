add8e09ab5b3b95b9b758d0c769fc0832572d27d
AJ4200 <abeljackson33@gmail.com>
2023-03-18 12:24:34 +0200

    first commit

---
+++ src/main/java/com/jeproductions/commonfunlib_api/CommonfunlibApiApplication.java
@@ -0,0 +1,13 @@
+package com.jeproductions.commonfunlib_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonfunlibApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonfunlibApiApplication.class, args);
+	}
+
+}
